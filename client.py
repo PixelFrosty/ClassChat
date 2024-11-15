70cde1aca3b4ea6e536afd51cf96da97fe1dbfd8
Ethan Gretna <pixelfrostypublic@gmail.com>
2024-11-15 03:13:42 -0600

    IO Multiplexing
    Redid IO multiplexing using select()
    for client AND server, use the readable return of select() to figure out
    if sockets are either ready for receiving data, ready to read input from
    stdin, or accept a connection
    TODO: Clean up with functions

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 from socket import socket, AF_INET, SOCK_STREAM
+from select import select
+from sys import stdin, stdout, exit
 
 serverName = ''
 serverPort = 12000
@@ -6,11 +8,30 @@ serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
-message = input("[You] ")
+sockets = [clientSocket, stdin]
 
-clientSocket.send(message.encode())
+while True:
+    readable, writeable, exceptional = select(sockets, sockets, [])
 
-confirmation = clientSocket.recv(1024)
-print("[Server] " + confirmation.decode())
-
-clientSocket.close()
+    for s in readable:
+        if s == clientSocket:
+            data = s.recv(1024).decode()
+            if data:
+                if data.strip().lower() == 'exit':
+                    stdout.write("Server closed, shutting down app")
+                    stdout.flush()
+                    exit()
+                stdout.write(f"[Server] {data}")
+                stdout.flush()
+            else:
+                s.close()
+        elif s == stdin:
+            message = stdin.readline()
+            clientSocket.send(message.encode())
+            stdout.write(f"[Client] {message}")
+            stdout.flush()
+            if message.strip().lower() == "exit":
+                stdout.write(f"Disconnecting from {clientSocket.getpeername()}")
+                stdout.flush()
+                clientSocket.close()
+                exit()
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from socket import socket, AF_INET, SOCK_STREAM
+from socket import SO_REUSEADDR, SOL_SOCKET, socket, AF_INET, SOCK_STREAM
+from select import select
+from sys import stdin, stdout, exit
 
 # 12000 = TCP socket
 # AF_INET for IPV4 address family, SOCK_STREAM for TCP
@@ -7,15 +9,57 @@ serverPort = 12000
 serverName = ''
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 serverSocket.bind((serverName,serverPort))
 
+sockets = [serverSocket, stdin]
+
 serverSocket.listen(1)
-print("The server is ready to recieve")
+print(f"The server is ready to recieve on {serverName if serverName != '' else 'localhost'} : {serverPort}")
 
 while True:
-        clientSocket, clientAddr = serverSocket.accept()
+    readable, writable, exceptional = select(sockets, [], [])
 
-        print("[Client] ", clientSocket.recv(1024).decode())
-        clientSocket.send("Message received".encode())
+    for s in readable:
+        if s == serverSocket:
+            clientSocket, clientAddr = serverSocket.accept()
+            stdout.write(f"\n{clientAddr} has connected to the server.\n\n")
+            stdout.flush()
+            clientSocket.send("Connection successful\n".encode())
 
-        clientSocket.close()
+            sockets.append(clientSocket)
+        elif s == stdin:
+            message = stdin.readline()
+            for c in sockets:
+                if c != serverSocket and c != stdin:
+                    c.send(message.encode())
+            stdout.write(f"[Server] {message}")
+            stdout.flush()
+            if message.strip().lower() == "exit":
+                stdout.write("Closing Server")
+                stdout.flush()
+                for c in sockets:
+                    if c != serverSocket and c != stdin:
+                        c.close()
+                        sockets.remove(c)
+                s.close()
+                exit()
+        else:
+            try:
+                data = s.recv(1024).decode()
+                if data:
+                    if data.strip().lower() == "exit":
+                        stdout.write(f"{s.getpeername()} is disconnecting\n")
+                        stdout.flush
+                        sockets.remove(s)
+                        continue
+                    stdout.write(f"[Client] {data}")
+                    stdout.flush
+                    s.send("Message received\n".encode())
+            except Exception:
+                continue
+
+    for s in exceptional:
+        print(f"Disconnecting {s.getpeername()} for exception")
+        sockets.remove(s)
+
